634f5aba1f73d72ec49feb3f65bb29cf971023da
vnasserb <83405997+vnasserb@users.noreply.github.com>
2023-08-25 14:59:49 -0300

    Update Buser.py

--- Buser.py
+++ Buser.py
@@ -6,33 +6,48 @@ from bs4 import BeautifulSoup
 def getText(text):
   return text.replace("\n","").strip()
 
-def findBusesBuser(slugOrigin, slugDestination, departureDay, departureMonth, departureYear, return_date_as_string = True, date_format = '%d/%m/%Y'):
+def findBusesBuser(slugOrigin, slugDestination, departureDay, departureMonth, departureYear, return_date_as_string = True):
   html = urlopen("https://www.buser.com.br/onibus/"+ slugOrigin +"/"+ slugDestination +"?ida=" + str(departureYear) + "-" + str(departureMonth) + "-" + str(departureDay))
   bs = BeautifulSoup(html, 'html.parser')
   busesList = bs.find_all('div', {'class': 'gci-header'})
-  keys = ['goingDay', 'arrivalDay', 'goingTime', 'arrivalTime', 'goingAdress', 'arrivalAdress', 'price', 'seat']
-  dictArray = []
-  stringToDate = lambda x: datetime.strptime(x, date_format)
 
+  keysDatetime = ['departureDatetime', 'arrivalDatetime', 'departureStation', 'arrivalStation', 'durationTime', 'serviceClass', 'price', 'company']
+  keysString = ['departureDay', 'arrivalDay', 'departureTime', 'arrivalStation', 'departureStation', 'arrivalAdress', 'durationTime', 'serviceClass', 'price', 'company']
+  stringToDate = lambda x: datetime.strptime(x, '%d/%m/%Y %H:%M:%S')
+  
+  dictArray = []
+  
   for bus in range(len(busesList)):
     try:
-      goingDayStr, arrivalDayStr = list(map(lambda x: x.text.replace("\n","").strip() + "/" + str(departureYear), busesList[bus].find_all('p', {'class': 'ird-dia'})))
+      departureDayStr, arrivalDayStr = list(map(lambda x: x.text.replace("\n","").strip() + "/" + str(departureYear), busesList[bus].find_all('p', {'class': 'ird-dia'})))
     except:
-      goingDayStr, arrivalDayStr = list(map(lambda x: x.text.replace("\n","").strip() + "/" + str(departureYear), 2 * busesList[bus].find_all('p', {'class': 'ird-dia'})))
+      departureDayStr, arrivalDayStr = list(map(lambda x: x.text.replace("\n","").strip() + "/" + str(departureYear), 2 * busesList[bus].find_all('p', {'class': 'ird-dia'})))
 
-    if return_date_as_string:
-      goingDay, arrivalDay = goingDayStr, arrivalDayStr
+    departureTime, arrivalTime = list(map(lambda x: x.text.replace("\n","").strip(), busesList[bus].find_all('p', {'class': 'ird-hora'})))
+
+    departureDatetime, arrivalDatetime = stringToDate(departureDayStr + " " + departureTime + ":00"), stringToDate(arrivalDayStr + " " + arrivalTime + ":00")
+
+    totalDuration = arrivalDatetime - departureDatetime
+    totalDurationMinutes = totalDuration.total_seconds() / 60
+    durationHours = totalDurationMinutes // 60
+    durationMinutes = totalDurationMinutes - durationHours * 60
+
+    if durationMinutes > 0:
+      duration = str(int(durationHours)) + "h " + str(int(durationMinutes)) + "m"
     else:
-      goingDay, arrivalDay = stringToDate(goingDayStr), stringToDate(arrivalDayStr)
+      duration = str(int(durationHours)) + "h"
 
-    goingTime, arrivalTime = list(map(lambda x: x.text.replace("\n","").strip(), busesList[bus].find_all('p', {'class': 'ird-hora'})))
-    goingAdress = getText(busesList[bus].find('div', {'class': 'ir-endereco is-origem'}).text).replace("Embarque:","").strip()
+    departureAdress = getText(busesList[bus].find('div', {'class': 'ir-endereco is-origem'}).text).replace("Embarque:","").strip()
     arrivalAdress = getText(busesList[bus].find('div', {'class': 'ir-endereco is-destino'}).text).replace("Desembarque:","").strip()
     price = float(getText(busesList[bus].find('div', {'class': 'preco'}).text)[3:].replace(",","."))
     seat = getText(busesList[bus].find('div', {'class': 'p-assento'}).text)
     # seatsQty = int(getText(busesList[bus].find('span', {'class': 'aviso-vagas-restantes'}).text)[:2].strip())
 
-    dictionary = dict(zip(keys, [goingDay, arrivalDay, goingTime, arrivalTime, goingAdress, arrivalAdress, price, seat]))
+    if return_date_as_string:
+      dictionary = dict(zip(keysString, [departureDayStr, arrivalDayStr, departureTime, arrivalTime, departureAdress, arrivalAdress, duration, seat, price, 'Buser']))
+    else:
+      dictionary = dict(zip(keysDatetime, [departureDatetime, arrivalDatetime, departureAdress, arrivalAdress, duration, seat, price, 'Buser']))
+
     dictArray.append(dictionary)
 
-  return pd.DataFrame(dictArray)
+  return dictArray
